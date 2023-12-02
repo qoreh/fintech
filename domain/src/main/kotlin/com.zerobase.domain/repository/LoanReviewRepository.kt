c14bcebf87946b10c9c577facf4bad484177ca25
qoreh <qoreh923@gmail.com>
2023-12-02 21:57:45 +0900

    .

---
+++ domain/src/main/kotlin/com.zerobase.domain/repository/LoanReviewRepository.kt
@@ -0,0 +1,10 @@
+package com.zerobase.domain.repository
+
+import com.zerobase.domain.domain.LoanReview
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+
+interface LoanReviewRepository : JpaRepository<LoanReview, Long> {
+    fun findByUserKey(userKey: String): LoanReview?
+}
---
+++ domain/src/main/kotlin/com.zerobase.domain/repository/UserInfoRepository.kt
@@ -0,0 +1,12 @@
+package com.zerobase.domain.repository
+
+import com.zerobase.domain.domain.UserInfo
+import org.springframework.context.annotation.Bean
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Component
+import org.springframework.stereotype.Repository
+
+
+interface UserInfoRepository : JpaRepository<UserInfo, Long> {
+    fun findByUserKey(userKey: String): UserInfo
+}
